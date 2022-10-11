80761a28a389a63b882455cd0104e73ef572757d
Jatin Agrawal <jatinagrawal0801@gmail.com>
2022-10-11 20:37:16 +0000

    first commit

---
+++ my-app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM  from 'react-dom';
+
+const element= <h1>Hello World</h1>
+
+const e1= <h1>Hello CSUN how are you?</h1>
+
+ReactDOM.render(element,document.getElementById("root"));
+ReactDOM.render(e1,document.getElementById("root"))
+
+// this statement would enter this element in the root element in html
+
--- readme.md
+++
@@ -1,1 +0,0 @@
-This a test repo. All the projects are tested here.So dont waste your time trying to understand the code. The code is all crap.
