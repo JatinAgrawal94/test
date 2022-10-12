9e89479863b3f49332d1ed302445ffeff504de24
Jatin Agrawal <jatinagrawal0801@gmail.com>
2022-10-12 05:16:30 +0000

    first commit

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import ReactDOM  from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const element= <h1>Hello World</h1>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-const e1= <h1>Hello CSUN how are you?</h1>
-
-ReactDOM.render(element,document.getElementById("root"));
-ReactDOM.render(e1,document.getElementById("root"))
-
-// this statement would enter this element in the root element in html
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
